037fb9ecbba11d145d6cc704b0746b54db2c2100
Lev Karatun <lev.karatun@gmail.com>
2020-05-10 22:39:51 -0400

    Fix user's events display

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { serverSideRequest } from "utils/request";
+import cookie from "cookie";
 import Profile from "../components/Profile";
 import MainLayout from "../components/MainLayout";
 
 export async function getServerSideProps({ req }) {
+  const userId = cookie.parse(req?.headers?.cookie).userId;
+  if (!userId) return { props: { userData: {}, events: [] } };
+
   const [userData, events] = await Promise.all([
     serverSideRequest(req)({ url: "users" }),
-    serverSideRequest(req)({ url: "events" })
+    serverSideRequest(req)({ url: `users/${userId}/events` })
   ])
     .then(arr => arr.map(i => i.data))
     .catch(console.log);
