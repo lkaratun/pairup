493fd7e354b243fe62d9896d7558109c6de8e89c
Lev Karatun <lev.karatun@gmail.com>
2020-06-01 23:23:55 -0400

    Clean up

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,5 +1,5 @@
 import { Resolver, Query, Mutation, Arg } from "type-graphql";
-import { User } from "../entity/User";
+import { User } from "../entities/User";
 import { CreateUserInput, UpdateUserInput } from "../inputs/UserInputs";
 
 @Resolver()
