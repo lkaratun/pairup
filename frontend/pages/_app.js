dfdf33b0fa92548e2a49721a76a662c22774f82b
Lev Karatun <lev.karatun@gmail.com>
2020-06-27 19:54:31 -0400

    Set up next-with-apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,21 +1,32 @@
 import React from "react";
-import cookie from "cookie";
-import UserProvider from "../components/UserProvider";
+import Cookie from "cookie";
+import withApollo from "next-with-apollo";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import "react-datepicker/dist/react-datepicker.css";
-import Cookies from "universal-cookie";
-import { omit } from "lodash";
+import App from "next/app";
+import { merge } from "lodash";
+import { UserProvider } from "../components/UserProvider";
 
-MyApp.getInitialProps = async function({ ctx }) {
-  const cookies = new Cookies(ctx?.req?.headers?.cookie).cookies;
-  return { props: { cookies: omit(cookies, "token") } };
+MyApp.getInitialProps = async function(appContext) {
+  const cookies = Cookie.parse(appContext?.ctx?.req?.headers?.cookie);
+  const appProps = await App.getInitialProps(appContext);
+  console.log("merge(appProps, { props: { cookies } })", merge(appProps, { props: { cookies } }));
+  return merge(appProps, { props: { cookies } });
 };
 
-function MyApp({ Component, props, pageProps }) {
+function MyApp({ Component, props, pageProps, apollo }) {
   return (
-    <UserProvider cookies={props.cookies}>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ApolloProvider client={apollo}>
+      <UserProvider cookies={props.cookies}>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ApolloProvider>
   );
 }
 
-export default MyApp;
+export default withApollo(({ initialState }) => {
+  return new ApolloClient({
+    uri: "http://api.local.pair-up.net:4000/graphql",
+    cache: new InMemoryCache().restore(initialState || {})
+  });
+})(MyApp);
