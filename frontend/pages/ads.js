b8d658351af3c2f0fa22819418a74accbe6ed33d
Lev Karatun <lev.karatun@gmail.com>
2020-06-29 00:05:45 -0400

    Create ads page

--- frontend/pages/ads.js
+++ frontend/pages/ads.js
@@ -1,27 +1,57 @@
-import React, { Component } from "react";
+import React from "react";
 import { gql } from "@apollo/client";
+import styled from "styled-components";
 import { initializeApollo } from "../lib/apolloClient";
+import Ad from "../components/Ad";
 
 export async function getServerSideProps(ctx) {
   const apolloClient = initializeApollo();
 
   const ads = await apolloClient.query({
     query: gql`
-      query GetUsers {
-        users {
+      query GetAds {
+        ads {
           id
-          firstName
+          description
+          activity {
+            id
+            name
+          }
+          responses {
+            id
+            user {
+              firstName
+            }
+          }
         }
       }
     `
   });
-  // .then(result => console.log("query result = ", result.data));
-  console.log("query result = ", ads.data);
-  return { props: { ads: ads.data } };
+  return { props: { ads: ads.data.ads } };
 }
 
 function Ads({ ads }) {
-  return `Ads page, ads = ${JSON.stringify(ads)} `;
+  return (
+    <Container>
+      <h1>Ads</h1>
+      {ads.map(ad => (
+        <Ad key={ad.id} ad={ad} />
+      ))}
+    </Container>
+  );
 }
 
 export default Ads;
+
+const h1 = styled.h1`
+  margin: auto;
+`;
+
+const Container = styled.div`
+  max-width: 80vw;
+  margin: 50px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
